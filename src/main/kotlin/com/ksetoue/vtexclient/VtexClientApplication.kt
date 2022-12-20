6a1ce7491c47a8054d149a10bce3c4e9c4a9d0a8
ksetoue <karoline.kimiko@gmail.com>
2022-12-19 21:48:52 -0300

    :tada: First commit

---
+++ src/main/kotlin/com/ksetoue/vtexclient/VtexClientApplication.kt
@@ -0,0 +1,11 @@
+package com.ksetoue.vtexclient
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VtexClientApplication
+
+fun main(args: Array<String>) {
+	runApplication<VtexClientApplication>(*args)
+}
